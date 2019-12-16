86c8ec771553361c774df866fe91e42729ff0ee5
Anthony Fajardo <anthonyfajardo@gmail.com>
2019-12-16 00:35:57 -0500

    changed menu id on functions.php, created global.scss

--- functions.php
+++ functions.php
@@ -44,7 +44,7 @@ if ( ! function_exists( 'jnrmillwork_setup' ) ) :
 
 		// This theme uses wp_nav_menu() in one location.
 		register_nav_menus( array(
-			'menu-1' => esc_html__( 'Primary', 'jnrmillwork' ),
+			'primary' => esc_html__( 'primary', 'jnrmillwork' ),
 		) );
 
 		/*
